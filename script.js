b2d97a312e230a84e869dfd2babf729328ef42ef
Ruthercloud Solutions <taruntelitech@gmail.com>
2025-12-22 13:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -281,5 +281,15 @@ function openMobileMenu() {
 
 
 
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  scrollBtn.style.display = window.scrollY > 120 ? "block" : "none";
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 
 
